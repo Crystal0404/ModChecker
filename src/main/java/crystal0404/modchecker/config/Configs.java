db8d9e29217890d07c7b6290141c2710f9e867d3
Crystal0404 <syh040325@163.com>
2024-11-27 23:29:26 +0800

    GPL-3.0

--- src/main/java/crystal0404/modchecker/config/Configs.java
+++ src/main/java/crystal0404/modchecker/config/Configs.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of the ModChecker project, licensed under the
- * GNU Lesser General Public License v3.0
+ * GNU General Public License v3.0
  *
  * Copyright (C) 2024  Crystal0404 and contributors
  *
  * ModChecker is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * ModChecker is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with ModChecker.  If not, see <https://www.gnu.org/licenses/>.
  */
 
